cb7d98b296b5b4170c415ee25641c781dc295909
Sara Gochez <saragochez13@gmail.com>
2022-01-03 23:11:57 -0800

    windows alert added

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+fight();
