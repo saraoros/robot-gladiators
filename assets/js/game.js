f560d69a17f6d7dfec6afb497340656e8ad8f96f
Sara Gochez <saragochez13@gmail.com>
2022-01-05 17:33:39 -0800

    added console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,8 +2,16 @@
 
 var playerName = window.prompt("What is your robot's name?");
 
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName);
+
 function fight() {
     window.alert("The fight has begun!")
 }
 
-fight();
+// fight();
